a33673c9c6d0e54bdb6845f3246d822535b4ee27
Alex Lopatin <alex.lopatin@tradingtechnologies.com>
2025-11-27 10:42:01 -0600

    shorten games to 1:30 minutes

--- common/src/constants.rs
+++ common/src/constants.rs
@@ -10,11 +10,11 @@ pub const DEFAULT_CUSTOM_GAME_TICK_MS: u32 = 100;
 /// Default available food target
 pub const DEFAULT_FOOD_TARGET: usize = 10;
 
-/// Default time limit for team games in milliseconds (3 minutes)
-pub const DEFAULT_TEAM_TIME_LIMIT_MS: u32 = 180_000;
+/// Default time limit for team games in milliseconds (1 minute 30 seconds)
+pub const DEFAULT_TEAM_TIME_LIMIT_MS: u32 = 90_000;
 
-/// Quickmatch time limit for team games in milliseconds (2 minutes)
-pub const DEFAULT_QUICKMATCH_TEAM_TIME_LIMIT_MS: u32 = 120_000;
+/// Quickmatch time limit for team games in milliseconds (1 minute 30 seconds)
+pub const DEFAULT_QUICKMATCH_TEAM_TIME_LIMIT_MS: u32 = 90_000;
 
 /// Default interval for cluster singleton renewal in milliseconds
 pub const CLUSTER_RENEWAL_INTERVAL_MS: u64 = 150;
